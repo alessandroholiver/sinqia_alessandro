31d64e257711bcfbde185fcd104a1a97692fe072
Alessandro Oliveira <alessandrocanela@gmail.com>
2019-09-04 15:55:13 -0300

    Cricção da pagina com a funçao somar

---
+++ Projeto03/script.js
@@ -0,0 +1,16 @@
+var comando = document.getElementById("btn").addEventListener
+('click', function(){
+    window.open("/pag2.html", '_blank');
+})
+
+
+function btnn(){
+    window.open("pag2.html", '_blank');
+
+}
+
+
+function voltar(){
+    window.open("index.html", '_blank');
+
+}
